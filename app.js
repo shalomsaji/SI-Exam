d9251d6418955e2811b799806098d77f25632335
shalomsaji <shalomsaji3@gmail.com>
2023-04-23 08:58:20 +0530

    Express Generator installed

--- app.js
+++ app.js
@@ -1,41 +1,41 @@
-const http = require('http');
-const fs = require('fs');
-const express = require('express');
-const url=require('url');
-/* var path = require('path')
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+
 var app = express();
 
-app.use(express.static(path.join(_dirname, 'public'))); */
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
 
-http.createServer((req, res) => {
-    
-    var q=url.parse(req.url, true);
-    console.log(q.pathname);
-    if (req.url === '/') { 
-        fs.readFile('index.html', (err, data) => {
-            res.writeHead(200, { 'Content-Type': 'text/html' });
-            res.write(data);
-            res.end();
-            })
-    }else if(req.url === '/register'){
-        fs.readFile('views/test.html', (err, data) => {
-            res.writeHead(200, { 'Content-Type': 'text/html' });
-            res.write(data);
-            res.end();
-            })
-    }else if(q.pathname === '/signupaction'){
-        console.log(q.query);
-        fs.readFile('./views/testquiz.html', (err, data) => {
-            res.writeHead(200, { 'Content-Type': 'text/html' });
-            res.write(data);
-            res.end();
-            })
-    }else{
-        res.writeHead(404,{ 'Content-Type': 'text/html' });
-        res.write('Error')
-        res.end();
-    }
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
 
-    
-}).listen(7000, () => { console.log('Started') });
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
 
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
