6e96026b69928b50619f48093a83d772216d9e61
shalomsaji <shalomsaji3@gmail.com>
2023-04-24 11:49:53 +0530

    Started Mongodb

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var quizRouter = require('./routes/quiz');
+var loginRouter = require('./routes/login');
 
 var app = express();
 
@@ -21,6 +23,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/quiz', quizRouter);
+app.use('/login', loginRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,20 @@
 var express = require('express');
 var router = express.Router();
+var MongoClient = require('mongodb').MongoClient;
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
-
+router.post('/register', (req,res)=>{
+  console.log(req.body)
+  MongoClient.connect('mongodb://localhost:27017',(err,client)=>{
+    if(err)
+      console.log('error')
+     else
+      console.log('Database Connected'); 
+  })
+  res.end();
+})
 module.exports = router;
