ac061e8d630fc45ef1403cd7d696ad2ad9b1f3fa
shalomsaji <shalomsaji3@gmail.com>
2023-04-25 07:24:32 +0530

    Index redesigned

--- routes/index.js
+++ routes/index.js
@@ -3,18 +3,23 @@ var router = express.Router();
 var MongoClient = require('mongodb').MongoClient;
 
 
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Quiz Login' });
 });
 router.post('/register', (req,res)=>{
   console.log(req.body)
-  MongoClient.connect('mongodb://localhost:27017',(err,client)=>{
-    if(err)
+  
+  MongoClient.connect("mongodb://localhost:27017/sample",function(err,client){
+    console.log('Test point')
+    console.log('Database Connected');
+    if (err)
       console.log('error')
-     else
-      console.log('Database Connected'); 
+     else 
+      client.db('sample').collection('user').insertOne(req.body)
   })
+  res.send('Got IT');
   res.end();
 })
 module.exports = router;
